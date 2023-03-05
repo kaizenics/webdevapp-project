fd9fbafa63fec9b9aca5c00b772d837bb5eca60f
kaizenics <nicosejohnsoriano@gmail.com>
2023-03-06 01:26:04 +0800

    Updated Files

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,43 @@
 import React from "react";
 
+
 function Contact() {
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
+      <form className="w-full max-w-lg">
+        <div className="flex flex-wrap mb-4">
+          <div className="w-full">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="name">
+              Name
+            </label>
+            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="name" type="text" placeholder="Your Name" />
+          </div>
+        </div>
+        <div className="flex flex-wrap mb-4">
+          <div className="w-full">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="email">
+              Email
+            </label>
+            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" placeholder="Your Email" />
+          </div>
+        </div>
+        <div className="flex flex-wrap mb-4">
+          <div className="w-full">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="message">
+              Message
+            </label>
+            <textarea className="no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="message" placeholder="Your Message" rows="5"></textarea>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+            Send Message
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
 
-export default Home;
+export default Contact;
